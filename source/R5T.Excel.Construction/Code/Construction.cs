0a57f71827ab80de2a7c1a8b3333b5dd49b95a31
MinexAutomation <minex.automation@gmail.com>
2019-07-15 15:24:52 -0400

    Initial commit of source-stub.

---
+++ source/R5T.Excel.Construction/Code/Construction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Excel.Construction
+{
+    public static class Construction
+    {
+        public static void SubMain()
+        {
+            Construction.SubMain();
+        }
+    }
+}
---
+++ source/R5T.Excel.Construction/Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Excel.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Construction.
+        }
+    }
+}
